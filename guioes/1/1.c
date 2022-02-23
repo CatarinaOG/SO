f534a2150f115b9818a46d8856af318a2d30c231
Catarina <a93259@alunos.uminho.pt>
2022-02-23 23:00:29 +0000

    Guiao 1

--- guioes/1/1.c
+++ guioes/1/1.c
@@ -1,64 +1,142 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-
-//--------------------------------------Exercicio 1---------------------------------
-
-/*int main (int argc, char** argv){
-
-    int buffer_size;
-
-    if(argc > 1) buffer_size = atoi(argv[1]);
-    else buffer_size = 10;
-
-    int fd_read = open("test", O_RDONLY | O_CREAT);
-
-    if(fd_read < 0)
-        perror("open error");
-
-    int fd_write = open("test1", O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-    ssize_t read_bytes = 0;
-    ssize_t written_bytes = 0;
-
-    char* buffer = malloc(sizeof(char) * buffer_size);
-
-    while(read_bytes = read(fd_read,buffer,buffer_size) > 0)
-        written_bytes = write(fd_write, buffer, read_bytes);
-
-
-    free(buffer);
-
-    return 0;
-}*/
+#include <stdio.h>
+#include <stdbool.h>
 
 
 
 
-//--------------------------------------Exercicio 2---------------------------------
 
+//---------------------------------Exercicio 1---------------------------------------
 
+void mycp(){
 
+    int readBytes;
+    int bufferSize = 1024;
+    char* buffer = malloc(sizeof(char) * bufferSize);
 
-int main( int argc, char** argv){
+    int fd_from = open("hello.txt", O_RDONLY | O_CREAT);
+    int fd_to = open("blank.txt", O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
+    if(fd_from <0 || fd_to < 0) printf("erro");
 
-    int buffer_size = 100;
-
-    ssize_t read_bytes = 0;
+    while( (readBytes = read(fd_from,buffer,bufferSize)) > 0 )
+        write(fd_to,buffer,readBytes);
     
+    free(buffer);
 
-    char* buffer = malloc(sizeof(char) * buffer_size);
+}
 
-    while(read_bytes = read(0,buffer,buffer_size) != 0)
-        write(1, buffer, read_bytes);
 
+//---------------------------------Exercicio 2---------------------------------------
+
+void mycat(){
+
+    int readBytes;
+    int bufferSize = 1024;
+    char* buffer = malloc(sizeof(char) * bufferSize);
+
+    while( (readBytes = read(0,buffer,bufferSize)) > 0 )
+        write(1,buffer,readBytes);
 
     free(buffer);
 
-    return 0;
 }
+
+//---------------------------------Exercicio 3---------------------------------------
+
+ssize_t readln(int fd, char *line, size_t size){
+    size_t i = 0;
+    while(i < size - 1){
+
+        size_t nbr = (size_t) read(fd,&line[i],sizeof(char));
+        if(nbr < 1) break;
+        if(line[i++] == '\n') break;
+    }
+
+    line[i] = '\0';
+    printf("%s",line);
+    return (ssize_t) i;
+}
+
+
+
+
+//---------------------------------Exercicio 4---------------------------------------
+
+void mynl(){
+
+    int ind = 1;
+    int readBytes;
+    int bufferSize = 1024;
+    char* buffer = malloc(sizeof(char) * bufferSize);
+
+    while( (readBytes = readln(0,buffer,bufferSize)) > 0 )
+        printf("%d: %s",ind++,buffer);
+    
+    free(buffer);
+}
+
+
+//---------------------------------Exercicio 5---------------------------------------
+
+void readln2(int fd, char *line, size_t size){
+
+    int readBytes;
+    int posLine = 0;
+    int continua = 1; 
+    int posBuffer;
+    int bufferSize = 1024;
+    char* buffer = malloc(sizeof(char)*bufferSize);
+
+    while( (readBytes = read(fd,buffer,bufferSize)) > 0){
+
+        for(int i=0 ; i<readBytes && continua; i++){
+            if(buffer[i] =='\n'){
+                posBuffer = i;
+                continua = 0;
+            }
+        }
+
+
+        for(int i=0 ; i<posBuffer ; i++){
+            line[posLine] = buffer[i];
+            posLine++;
+        }
+
+        lseek(fd,posBuffer-bufferSize,SEEK_CUR);
+    }
+}
+
+
+void mainReadln(){
+
+    int bufferSize = 1024;
+    char* buffer = malloc(sizeof(char) * bufferSize);
+
+    int fd_from = open("hello.txt", O_RDONLY | O_CREAT, 0600);
+
+    readln(fd_from,buffer,bufferSize);
+    //readln2(fd_from,buffer,bufferSize);
+
+}
+
+
+
+//---------------------------------Main---------------------------------------
+
+int main (int argc, char** argv){
+
+    //mycp();
+    //mycat();
+    //mainReadln();
+    //mynl();
+    mainReadln();
+
+}
+
+
+
+
